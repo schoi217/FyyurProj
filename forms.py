69cd61103b31fe72874e965a2c3914f617f7f47f
Seoho Choi <seohochoi1@gmail.com>
2022-03-06 11:35:12 -0800

    forms phone todo

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
-from wtforms.validators import DataRequired, AnyOf, URL
+from wtforms.validators import DataRequired, AnyOf, URL, Regexp
 
 class ShowForm(Form):
     artist_id = StringField(
@@ -83,13 +83,14 @@ class VenueForm(Form):
         'address', validators=[DataRequired()]
     )
     phone = StringField(
-        'phone'
+        'phone', validators=[Regexp('^\d{3}\-\d{3}-\d{4}$',
+                             message='Phone number is in the wrong format!' +
+                             ' We need the format xxx-xxx-xxxx please.')]
     )
     image_link = StringField(
         'image_link'
     )
     genres = SelectMultipleField(
-        # TODO implement enum restriction
         'genres', validators=[DataRequired()],
         choices=[
             ('Alternative', 'Alternative'),
@@ -192,8 +193,9 @@ class ArtistForm(Form):
         ]
     )
     phone = StringField(
-        # TODO implement validation logic for state
-        'phone'
+        'phone', validators=[Regexp('^\d{3}\-\d{3}-\d{4}$',
+                               message='Phone number is in the wrong format!' +
+                               ' We need the format xxx-xxx-xxxx please.')]
     )
     image_link = StringField(
         'image_link'
